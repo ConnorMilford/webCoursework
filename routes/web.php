61487e1117a1a6498ed2ff9c528f41e35c58448b
Connor <connormilford@hotmail.com>
2024-11-26 15:37:56 +0000

    First commit in new branch 'Controllers'. Added UserAccountController.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// https://canvas.swansea.ac.uk/courses/52735/files/7256039?module_item_id=3001447
+Route::get('/home/{name}', function ($name) {
+    return "Welcome to $name's home page";
+});
