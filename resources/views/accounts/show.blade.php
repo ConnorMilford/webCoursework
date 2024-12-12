d91a277f0eb3bbe6220bf58929c47a013a3a7895
Connor <connormilford@hotmail.com>
2024-12-12 19:12:25 +0000

    Created branch for working on profiles and Notifications

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -59,6 +59,11 @@
                         <button type="button" class="submit-post bg-transparent  text-white px-4 py-2 rounded mt-2">Submit</button>
                     </form>
                 </div>
+                
+                <!-- Pagination links -->
+                <div class="mt-4 text-center">
+                    {{$user ->posts->links()}}
+                </div>
             </div>
         </div>
     </div>
